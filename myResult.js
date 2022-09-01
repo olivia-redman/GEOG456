86b150a971aa0397c78beb6ad153f54b13b01a1d
olivia-redman <oliviare@live.unc.edu>
2022-09-01 01:03:00 -0400

    Create myResult.js

---
+++ myResult.js
@@ -0,0 +1,789 @@
+var myResult =
+[
+    {
+      "Year": "1935",
+      "lDate": "3-Feb",
+      "lTime": "2400",
+      "lElevft": "673.5",
+      "hDate": "31-Jul",
+      "hTime": "2400",
+      "hElevft": "928.45"
+    },
+    {
+      "Year": "1936",
+      "lDate": "14-Apr",
+      "lTime": "2400",
+      "lElevft": "905.2",
+      "hDate": "10-Sep",
+      "hTime": "2400",
+      "hElevft": "1025.87"
+    },
+    {
+      "Year": "1937",
+      "lDate": "5-Feb",
+      "lTime": "2400",
+      "lElevft": "1021.9",
+      "hDate": "28-Jul",
+      "hTime": "2400",
+      "hElevft": "1102.88"
+    },
+    {
+      "Year": "1938",
+      "lDate": "17-Feb",
+      "lTime": "1",
+      "lElevft": "1094.61",
+      "hDate": "29-Jul",
+      "hTime": "1",
+      "hElevft": "1173.56"
+    },
+    {
+      "Year": "1939",
+      "lDate": "18-Mar",
+      "lTime": "1",
+      "lElevft": "1156.1",
+      "hDate": "2-Jul",
+      "hTime": "1",
+      "hElevft": "1183.5"
+    },
+    {
+      "Year": "1940",
+      "lDate": "5-Apr",
+      "lTime": "1",
+      "lElevft": "1164.21",
+      "hDate": "3-Jul",
+      "hTime": "700",
+      "hElevft": "1182.21"
+    },
+    {
+      "Year": "1941",
+      "lDate": "16-Feb",
+      "lTime": "2400",
+      "lElevft": "1166.74",
+      "hDate": "30-Jul",
+      "hTime": "730",
+      "hElevft": "1220.46"
+    },
+    {
+      "Year": "1942",
+      "lDate": "28-Mar",
+      "lTime": "2400",
+      "lElevft": "1171.04",
+      "hDate": "8-Jul",
+      "hTime": "1200",
+      "hElevft": "1213.46"
+    },
+    {
+      "Year": "1943",
+      "lDate": "5-Apr",
+      "lTime": "1900",
+      "lElevft": "1176.71",
+      "hDate": "14-Jul",
+      "hTime": "1200",
+      "hElevft": "1202.45"
+    },
+    {
+      "Year": "1944",
+      "lDate": "8-Apr",
+      "lTime": "1900",
+      "lElevft": "1157.17",
+      "hDate": "19-Jul",
+      "hTime": "930",
+      "hElevft": "1200.36"
+    },
+    {
+      "Year": "1945",
+      "lDate": "26-Apr",
+      "lTime": "2400",
+      "lElevft": "1146.54",
+      "hDate": "21-Aug",
+      "hTime": "430",
+      "hElevft": "1182.5"
+    },
+    {
+      "Year": "1946",
+      "lDate": "19-Apr",
+      "lTime": "2100",
+      "lElevft": "1146.5",
+      "hDate": "7-Jul",
+      "hTime": "1500",
+      "hElevft": "1164.32"
+    },
+    {
+      "Year": "1947",
+      "lDate": "26-Apr",
+      "lTime": "300",
+      "lElevft": "1133.9",
+      "hDate": "2-Sep",
+      "hTime": "600",
+      "hElevft": "1180.27"
+    },
+    {
+      "Year": "1948",
+      "lDate": "30-Mar",
+      "lTime": "2100",
+      "lElevft": "1154.45",
+      "hDate": "11-Jul",
+      "hTime": "1800",
+      "hElevft": "1192.8"
+    },
+    {
+      "Year": "1949",
+      "lDate": "15-Apr",
+      "lTime": "2400",
+      "lElevft": "1145.18",
+      "hDate": "31-Jul",
+      "hTime": "1800",
+      "hElevft": "1196.68"
+    },
+    {
+      "Year": "1950",
+      "lDate": "13-Apr",
+      "lTime": "2400",
+      "lElevft": "1149.95",
+      "hDate": "24-Jul",
+      "hTime": "300",
+      "hElevft": "1177.66"
+    },
+    {
+      "Year": "1951",
+      "lDate": "11-May",
+      "lTime": "2400",
+      "lElevft": "1141.19",
+      "hDate": "13-Aug",
+      "hTime": "1200",
+      "hElevft": "1169.01"
+    },
+    {
+      "Year": "1952",
+      "lDate": "3-Apr",
+      "lTime": "2100",
+      "lElevft": "1133.24",
+      "hDate": "17-Jul",
+      "hTime": "900",
+      "hElevft": "1201.13"
+    },
+    {
+      "Year": "1953",
+      "lDate": "31-Dec",
+      "lTime": "2400",
+      "lElevft": "1145.78",
+      "hDate": "1-Jan",
+      "hTime": "2400",
+      "hElevft": "1169.13"
+    },
+    {
+      "Year": "1954",
+      "lDate": "31-Dec",
+      "lTime": "2400",
+      "lElevft": "1105.48",
+      "hDate": "1-Jan",
+      "hTime": "300",
+      "hElevft": "1145.83"
+    },
+    {
+      "Year": "1955",
+      "lDate": "22-Apr",
+      "lTime": "2400",
+      "lElevft": "1089.39",
+      "hDate": "7-Jul",
+      "hTime": "600",
+      "hElevft": "1106.63"
+    },
+    {
+      "Year": "1956",
+      "lDate": "26-Apr",
+      "lTime": "2100",
+      "lElevft": "1083.19",
+      "hDate": "9-Jul",
+      "hTime": "600",
+      "hElevft": "1116.84"
+    },
+    {
+      "Year": "1957",
+      "lDate": "20-Apr",
+      "lTime": "900",
+      "lElevft": "1089.48",
+      "hDate": "9-Sep",
+      "hTime": "1200",
+      "hElevft": "1184.11"
+    },
+    {
+      "Year": "1958",
+      "lDate": "19-Apr",
+      "lTime": "2400",
+      "lElevft": "1161",
+      "hDate": "7-Jul",
+      "hTime": "600",
+      "hElevft": "1205.93"
+    },
+    {
+      "Year": "1959",
+      "lDate": "31-Dec",
+      "lTime": "1800",
+      "lElevft": "1167.11",
+      "hDate": "1-Jan",
+      "hTime": "600",
+      "hElevft": "1185.93"
+    },
+    {
+      "Year": "1960",
+      "lDate": "11-Mar",
+      "lTime": "2400",
+      "lElevft": "1162.97",
+      "hDate": "5-Jul",
+      "hTime": "600",
+      "hElevft": "1184.19"
+    },
+    {
+      "Year": "1961",
+      "lDate": "18-May",
+      "lTime": "1800",
+      "lElevft": "1152.78",
+      "hDate": "2-Jan",
+      "hTime": "600",
+      "hElevft": "1165.21"
+    },
+    {
+      "Year": "1962",
+      "lDate": "9-Feb",
+      "lTime": "2400",
+      "lElevft": "1153.16",
+      "hDate": "23-Jul",
+      "hTime": "1200",
+      "hElevft": "1204.21"
+    },
+    {
+      "Year": "1963",
+      "lDate": "31-Dec",
+      "lTime": "2400",
+      "lElevft": "1136.93",
+      "hDate": "1-Jan",
+      "hTime": "2400",
+      "hElevft": "1193.19"
+    },
+    {
+      "Year": "1964",
+      "lDate": "30-Dec",
+      "lTime": "2400",
+      "lElevft": "1088.09",
+      "hDate": "1-Jan",
+      "hTime": "2400",
+      "hElevft": "1136.93"
+    },
+    {
+      "Year": "1965",
+      "lDate": "3-Apr",
+      "lTime": "2400",
+      "lElevft": "1088.07",
+      "hDate": "31-Dec",
+      "hTime": "2400",
+      "hElevft": "1129.74"
+    },
+    {
+      "Year": "1966",
+      "lDate": "17-Sep",
+      "lTime": "2400",
+      "lElevft": "1127.18",
+      "hDate": "19-Feb",
+      "hTime": "1200",
+      "hElevft": "1133.84"
+    },
+    {
+      "Year": "1967",
+      "lDate": "22-Nov",
+      "lTime": "2400",
+      "lElevft": "1127.75",
+      "hDate": "13-Feb",
+      "hTime": "1200",
+      "hElevft": "1133.85"
+    },
+    {
+      "Year": "1968",
+      "lDate": "1-Jan",
+      "lTime": "1",
+      "lElevft": "1129.84",
+      "hDate": "31-Dec",
+      "hTime": "2400",
+      "hElevft": "1139.65"
+    },
+    {
+      "Year": "1969",
+      "lDate": "16-Jan",
+      "lTime": "2400",
+      "lElevft": "1139.38",
+      "hDate": "31-Dec",
+      "hTime": "2400",
+      "hElevft": "1152.5"
+    },
+    {
+      "Year": "1970",
+      "lDate": "20-Aug",
+      "lTime": "2400",
+      "lElevft": "1150.39",
+      "hDate": "21-Jan",
+      "hTime": "1200",
+      "hElevft": "1154.18"
+    },
+    {
+      "Year": "1971",
+      "lDate": "2-Apr",
+      "lTime": "2400",
+      "lElevft": "1148.05",
+      "hDate": "31-Dec",
+      "hTime": "2400",
+      "hElevft": "1160.13"
+    },
+    {
+      "Year": "1972",
+      "lDate": "25-May",
+      "lTime": "2400",
+      "lElevft": "1153.97",
+      "hDate": "31-Dec",
+      "hTime": "2400",
+      "hElevft": "1168.35"
+    },
+    {
+      "Year": "1973",
+      "lDate": "1-Jan",
+      "lTime": "1",
+      "lElevft": "1168.35",
+      "hDate": "7-May",
+      "hTime": "600",
+      "hElevft": "1187.01"
+    },
+    {
+      "Year": "1974",
+      "lDate": "15-Jun",
+      "lTime": "600",
+      "lElevft": "1168.81",
+      "hDate": "4-Feb",
+      "hTime": "600",
+      "hElevft": "1180.23"
+    },
+    {
+      "Year": "1975",
+      "lDate": "29-May",
+      "lTime": "1200",
+      "lElevft": "1173.61",
+      "hDate": "14-Oct",
+      "hTime": "600",
+      "hElevft": "1181.03"
+    },
+    {
+      "Year": "1976",
+      "lDate": "10-Oct",
+      "lTime": "1200",
+      "lElevft": "1177.58",
+      "hDate": "31-Dec",
+      "hTime": "2400",
+      "hElevft": "1188.27"
+    },
+    {
+      "Year": "1977",
+      "lDate": "6-Jul",
+      "lTime": "1200",
+      "lElevft": "1175.8",
+      "hDate": "6-Feb",
+      "hTime": "600",
+      "hElevft": "1193.82"
+    },
+    {
+      "Year": "1978",
+      "lDate": "1-Jan",
+      "lTime": "2400",
+      "lElevft": "1180.82",
+      "hDate": "31-Dec",
+      "hTime": "2400",
+      "hElevft": "1193.31"
+    },
+    {
+      "Year": "1979",
+      "lDate": "1-Jan",
+      "lTime": "2400",
+      "lElevft": "1193.37",
+      "hDate": "26-Feb",
+      "hTime": "600",
+      "hElevft": "1202.85"
+    },
+    {
+      "Year": "1980",
+      "lDate": "4-Jan",
+      "lTime": "2400",
+      "lElevft": "1197.64",
+      "hDate": "29-Sep",
+      "hTime": "600",
+      "hElevft": "1205.05"
+    },
+    {
+      "Year": "1981",
+      "lDate": "11-Sep",
+      "lTime": "1800",
+      "lElevft": "1192.44",
+      "hDate": "18-Feb",
+      "hTime": "600",
+      "hElevft": "1204.76"
+    },
+    {
+      "Year": "1982",
+      "lDate": "5-Aug",
+      "lTime": "2400",
+      "lElevft": "1196.25",
+      "hDate": "31-Dec",
+      "hTime": "2400",
+      "hElevft": "1208.37"
+    },
+    {
+      "Year": "1983",
+      "lDate": "31-Jan",
+      "lTime": "2400",
+      "lElevft": "1206.83",
+      "hDate": "24-Jul",
+      "hTime": "2200",
+      "hElevft": "1225.85"
+    },
+    {
+      "Year": "1984",
+      "lDate": "4-May",
+      "lTime": "2400",
+      "lElevft": "1205.73",
+      "hDate": "23-Jul",
+      "hTime": "1200",
+      "hElevft": "1213.73"
+    },
+    {
+      "Year": "1985",
+      "lDate": "31-Dec",
+      "lTime": "2400",
+      "lElevft": "1205.49",
+      "hDate": "8-Jul",
+      "hTime": "600",
+      "hElevft": "1214.36"
+    },
+    {
+      "Year": "1986",
+      "lDate": "24-Jan",
+      "lTime": "2400",
+      "lElevft": "1201.42",
+      "hDate": "2-Oct",
+      "hTime": "2400",
+      "hElevft": "1213.2"
+    },
+    {
+      "Year": "1987",
+      "lDate": "17-Jun",
+      "lTime": "2400",
+      "lElevft": "1205.6",
+      "hDate": "12-Feb",
+      "hTime": "2400",
+      "hElevft": "1210.83"
+    },
+    {
+      "Year": "1988",
+      "lDate": "29-Sep",
+      "lTime": "2400",
+      "lElevft": "1199.14",
+      "hDate": "28-Feb",
+      "hTime": "2400",
+      "hElevft": "1211.82"
+    },
+    {
+      "Year": "1989",
+      "lDate": "30-Nov",
+      "lTime": "2400",
+      "lElevft": "1189.02",
+      "hDate": "26-Feb",
+      "hTime": "600",
+      "hElevft": "1202.57"
+    },
+    {
+      "Year": "1990",
+      "lDate": "24-Dec",
+      "lTime": "1200",
+      "lElevft": "1177.87",
+      "hDate": "5-Mar",
+      "hTime": "600",
+      "hElevft": "1191.88"
+    },
+    {
+      "Year": "1991",
+      "lDate": "24-Sep",
+      "lTime": "2400",
+      "lElevft": "1172.7",
+      "hDate": "24-Feb",
+      "hTime": "2400",
+      "hElevft": "1180.19"
+    },
+    {
+      "Year": "1992",
+      "lDate": "21-Aug",
+      "lTime": "2400",
+      "lElevft": "1173.39",
+      "hDate": "24-Mar",
+      "hTime": "2400",
+      "hElevft": "1180.55"
+    },
+    {
+      "Year": "1993",
+      "lDate": "1-Jan",
+      "lTime": "2400",
+      "lElevft": "1176.9",
+      "hDate": "12-Apr",
+      "hTime": "2400",
+      "hElevft": "1193.75"
+    },
+    {
+      "Year": "1994",
+      "lDate": "30-Nov",
+      "lTime": "2400",
+      "lElevft": "1175.67",
+      "hDate": "27-Feb",
+      "hTime": "2400",
+      "hElevft": "1190.98"
+    },
+    {
+      "Year": "1995",
+      "lDate": "1-Jan",
+      "lTime": "100",
+      "lElevft": "1176.52",
+      "hDate": "31-Dec",
+      "hTime": "2000",
+      "hElevft": "1190.96"
+    },
+    {
+      "Year": "1996",
+      "lDate": "14-Aug",
+      "lTime": "2000",
+      "lElevft": "1189.68",
+      "hDate": "15-Apr",
+      "hTime": "1100",
+      "hElevft": "1195.05"
+    },
+    {
+      "Year": "1997",
+      "lDate": "1-Jan",
+      "lTime": "200",
+      "lElevft": "1194.36",
+      "hDate": "31-Dec",
+      "hTime": "1000",
+      "hElevft": "1214.65"
+    },
+    {
+      "Year": "1998",
+      "lDate": "29-May",
+      "lTime": "2300",
+      "lElevft": "1211.37",
+      "hDate": "15-Sep",
+      "hTime": "1000",
+      "hElevft": "1215.95"
+    },
+    {
+      "Year": "1999",
+      "lDate": "30-Jun",
+      "lTime": "2100",
+      "lElevft": "1206.4",
+      "hDate": "31-Dec",
+      "hTime": "2000",
+      "hElevft": "1213.96"
+    },
+    {
+      "Year": "2000",
+      "lDate": "13-Dec",
+      "lTime": "2300",
+      "lElevft": "1195.67",
+      "hDate": "10-Jan",
+      "hTime": "1400",
+      "hElevft": "1214.14"
+    },
+    {
+      "Year": "2001",
+      "lDate": "20-Dec",
+      "lTime": "2100",
+      "lElevft": "1177.18",
+      "hDate": "6-Feb",
+      "hTime": "900",
+      "hElevft": "1197.47"
+    },
+    {
+      "Year": "2002",
+      "lDate": "31-Dec",
+      "lTime": "1900",
+      "lElevft": "1152.1",
+      "hDate": "28-Jan",
+      "hTime": "400",
+      "hElevft": "1178.04"
+    },
+    {
+      "Year": "2003",
+      "lDate": "30-Dec",
+      "lTime": "2300",
+      "lElevft": "1139.04",
+      "hDate": "8-Mar",
+      "hTime": "1700",
+      "hElevft": "1154.63"
+    },
+    {
+      "Year": "2004",
+      "lDate": "31-Jul",
+      "lTime": "2100",
+      "lElevft": "1125.69",
+      "hDate": "25-Jan",
+      "hTime": "1800",
+      "hElevft": "1140.58"
+    },
+    {
+      "Year": "2005",
+      "lDate": "1-Jan",
+      "lTime": "1",
+      "lElevft": "1130.01",
+      "hDate": "29-Mar",
+      "hTime": "700",
+      "hElevft": "1147.8"
+    },
+    {
+      "Year": "2006",
+      "lDate": "3-Oct",
+      "lTime": "2300",
+      "lElevft": "1125.14",
+      "hDate": "27-Feb",
+      "hTime": "900",
+      "hElevft": "1141.39"
+    },
+    {
+      "Year": "2007",
+      "lDate": "4-Oct",
+      "lTime": "2300",
+      "lElevft": "1110.18",
+      "hDate": "14-Feb",
+      "hTime": "500",
+      "hElevft": "1130.03"
+    },
+    {
+      "Year": "2008",
+      "lDate": "31-Jul",
+      "lTime": "2300",
+      "lElevft": "1104.41",
+      "hDate": "11-Mar",
+      "hTime": "700",
+      "hElevft": "1117.96"
+    },
+    {
+      "Year": "2009",
+      "lDate": "6-Nov",
+      "lTime": "2300",
+      "lElevft": "1093.2",
+      "hDate": "25-Jan",
+      "hTime": "2100",
+      "hElevft": "1112.66"
+    },
+    {
+      "Year": "2010",
+      "lDate": "27-Nov",
+      "lTime": "2100",
+      "lElevft": "1081.85",
+      "hDate": "26-Feb",
+      "hTime": "200",
+      "hElevft": "1103.35"
+    },
+    {
+      "Year": "2011",
+      "lDate": "2-Jan",
+      "lTime": "2100",
+      "lElevft": "1086.24",
+      "hDate": "31-Dec",
+      "hTime": "1800",
+      "hElevft": "1132.83"
+    },
+    {
+      "Year": "2012",
+      "lDate": "21-Nov",
+      "lTime": "0",
+      "lElevft": "1114.98",
+      "hDate": "22-Jan",
+      "hTime": "1700",
+      "hElevft": "1134.56"
+    },
+    {
+      "Year": "2013",
+      "lDate": "12-Nov",
+      "lTime": "2200",
+      "lElevft": "1103.79",
+      "hDate": "5-Feb",
+      "hTime": "400",
+      "hElevft": "1122.72"
+    },
+    {
+      "Year": "2014",
+      "lDate": "13-Aug",
+      "lTime": "2000",
+      "lElevft": "1080.19",
+      "hDate": "2-Feb",
+      "hTime": "1",
+      "hElevft": "1108.96"
+    },
+    {
+      "Year": "2015",
+      "lDate": "26-Jun",
+      "lTime": "2300",
+      "lElevft": "1074.71",
+      "hDate": "22-Jan",
+      "hTime": "800",
+      "hElevft": "1089.32"
+    },
+    {
+      "Year": "2016",
+      "lDate": "1-Jul",
+      "lTime": "2300",
+      "lElevft": "1071.61",
+      "hDate": "18-Feb",
+      "hTime": "500",
+      "hElevft": "1084.46"
+    },
+    {
+      "Year": "2017",
+      "lDate": "2-Aug",
+      "lTime": "2200",
+      "lElevft": "1078.89",
+      "hDate": "1-Mar",
+      "hTime": "200",
+      "hElevft": "1089.8"
+    },
+    {
+      "Year": "2018",
+      "lDate": "11-Jul",
+      "lTime": "2300",
+      "lElevft": "1076.38",
+      "hDate": "12-Mar",
+      "hTime": "1100",
+      "hElevft": "1088.35"
+    },
+    {
+      "Year": "2019",
+      "lDate": "1-Jan",
+      "lTime": "0",
+      "lElevft": "1081.47",
+      "hDate": "31-Dec",
+      "hTime": "2300",
+      "hElevft": "1090.49"
+    },
+    {
+      "Year": "2020",
+      "lDate": "26-Nov",
+      "lTime": "2300",
+      "lElevft": "1081.04",
+      "hDate": "30-Mar",
+      "hTime": "1600",
+      "hElevft": "1098.71"
+    },
+    {
+      "Year": "2021",
+      "lDate": "2-Dec",
+      "lTime": "1700",
+      "lElevft": "1064.91",
+      "hDate": "14-Feb",
+      "hTime": "1400",
+      "hElevft": "1087.43"
+    },
+    {
+      "Year": ""
+    }
+  ]
